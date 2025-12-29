0a99ce404c920d2a83ade8efaba96a52add8112e
omarqouqas <omar.qouqas@gmail.com>
2025-12-28 23:42:26 -0500

    updated landing header

--- components/landing/landing-header.tsx
+++ components/landing/landing-header.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { Menu, X } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 import { GetStartedCTA } from '@/components/landing/get-started-cta';
 
 const NAV_LINKS = [
